ad5c844b58bcb84d425c38be97dcf0148e4edc84
Abidev <113099224+AbiDev11@users.noreply.github.com>
2023-02-01 19:43:31 +0700

    Delete plugins directory

--- plugins/oy.js
+++
@@ -1,7 +0,0 @@
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-await m.reply('Kiyomasee🗿')
-}
-handler.customPrefix = /^(oy|oyy|ooy)$/i
-handler.command = new RegExp
-
-export default handler
